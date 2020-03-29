e9596d07a711ce1630ab81fae0f3046887f5d16b
TecnoNX <40416331+TecnoNX@users.noreply.github.com>
2020-03-29 01:04:56 +0100

    nav

--- js/javascrip.js
+++ js/javascrip.js
@@ -1,3 +1,11 @@
+function cambiarClase(){
+  let siteNav = document.getElementById('site-nav');
+      siteNav.classList.toggle('site-nav-open');
+  let menuOpen = document.getElementById('menu-toggle');
+      menuOpen.classList.toggle('menu-open');    
+      
+}
+
 particlesJS(
     {
         "particles": {
